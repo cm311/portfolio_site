0ca8e085d8684d05f9ed3b05ca181b2a08f35821
Colin Moriarity <cm311@student.london.ac.uk>
2023-03-20 11:47:36 -0400

    styling and animations

--- front_page/urls.py
+++ front_page/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from django.http import HttpResponse
 
 
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('resume/', views.resume, name="resume")
 ]
--- front_page/views.py
+++ front_page/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import FileResponse
+from portfolio_site.settings import STATICFILES_DIRS as staticdirs
 
 # Create your views here.
 def home(request):
     return render(request, 'front_page/index.html')
+
+def resume(request):
+
+    file_location = staticdirs[0] + '\\Resume.pdf'
+    response = FileResponse(open(file_location, 'rb'))
+    return response
