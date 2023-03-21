3cba2f48aaeaf6d2e344ab64e9afb1cfd2fded31
Colin Moriarity <cm311@student.london.ac.uk>
2023-03-21 14:54:44 -0400

    changed some wording, updated resume

--- front_page/views.py
+++ front_page/views.py
@@ -2,13 +2,16 @@
 from django.shortcuts import render
 from django.http import FileResponse
 from portfolio_site.settings import STATICFILES_DIRS as staticdirs
+import os
 
 # Create your views here.
 def home(request):
     return render(request, 'front_page/index.html')
 
 def resume(request):
-
-    file_location = staticdirs[0] + '/Resume.pdf'
+    if os.name == 'nt':
+        file_location = staticdirs[0] + '\\Resume.pdf'
+    else:
+        file_location = staticdirs[0] + '/Resume.pdf'
     response = FileResponse(open(file_location, 'rb'))
     return response
